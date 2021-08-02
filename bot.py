f670733c2abb3a3bb6e118732ffac65c5796351e
__0o0_ <abcset6@gmail.com>
2021-08-02 13:03:58 +0800

    New changes

--- bot.py
+++ bot.py
@@ -36,6 +36,5 @@ for Filename in os.listdir('./cmds'):
     if Filename.endswith('.py'):
         bot.load_extension(f'cmds.{Filename[:-3]}')
 
-
 if __name__ == "__main__":
     bot.run(jdata['TOKEN'])
--- cmds/plan.py
+++ cmds/plan.py
@@ -6,16 +6,19 @@ import json
 with open('setting.json', mode='r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
-
-
 class Plan(Cog_Extension):
     @commands.command()
     async def revise(self, ctx):
         await ctx.send(jdata['REVISE'])
+
     @commands.command()
-    async def score_change(self, ctx, value:int):
-        score = score + value
-        await ctx.send(score)
+    async def score(self, ctx, score:int, value:int):
+        score += value
+        await ctx.channel.purge(limit=1)
+        await ctx.send('Your score is {} now!'.format(score))
+
+
+
 
 def setup(bot):
     bot.add_cog(Plan(bot))
